18772bac06dc50e501bffa43f55eb06ad42489bc
Cristhina <viszellechacon@gmail.com>
2025-10-05 22:27:35 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,15 @@ app.use(cors())
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const server = require('http').createServer(app)
 
 const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST']
   }
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
