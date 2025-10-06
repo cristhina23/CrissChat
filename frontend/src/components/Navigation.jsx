b99eb339764f8e3713377de8686c5356c5132d13
Cristhina <viszellechacon@gmail.com>
2025-10-06 00:44:50 -0500

    update

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -2,17 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {LinkContainer} from 'react-router-bootstrap';
+import favicon from "/src/assets/favicon-96x96.png";
 
 function Navigation() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to="/">
+        <Navbar.Brand> 
+          <img src={favicon} alt="logo image of crisschat app" style={{width: "70px", height: "70px"}}/> 
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto">
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/chat">
+              <Nav.Link>Chat</Nav.Link>
+            </LinkContainer>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Row, Col, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import '../styles/Home.css'
+import HomeBg from '../assets/home-bg.png'
 
 function Home() {
   return (
-    <div>Home</div>
+    <Row>
+      <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
+        <div className='home__content'>
+          <h1 className='home__title'>Welcome to 
+            <span className='cool-gradient-text'> CrissChat</span>
+            </h1>
+          <p className='home__subtitle'>Chat with your friends and family anywhere in the world</p>
+          <LinkContainer to="/chat">
+            <Button className='btn btn-style-secondary'>Get Started <i className='fas fa-comments home-message-icon'></i></Button>
+            
+          </LinkContainer>
+        </div>
+      </Col>
+      <Col md={6} className='home__bg'> 
+        <img src={HomeBg} alt="" />
+      </Col>
+    </Row>
   )
 }
 
