b67941c7c26260e3bb6b276392d74941042ad6c2
Cristhina <viszellechacon@gmail.com>
2025-10-05 22:04:06 -0500

    update

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+const rooms = ['general', 'tech', 'finance', 'crypto'];
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
